fdfb4194fcd6dc4af80acbddef66cabf6583e0e4
ftxprox <168733166+ftxprox@users.noreply.github.com>
2024-09-17 13:45:24 +0000

    Deploying to gh-pages from @ ftxprox/goit-js-hw-11@a18a169cb5152f727d8b7b74d6045f9f860d8dbb 🚀

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ import{S as u,i as a}from"./assets/vendor-5ObWk2rO.js";(function(){const n=docum
           src="${e.webformatURL}"
           alt="${e.tags}" 
           loading="lazy" />
+
         <ul class="list-wrapper">
           <li class="text-content"><b>Likes:</b> ${e.likes}</li>
           <li class="text-content"><b>Views:</b> ${e.views}</li>
           <li class="text-content"><b>Comments:</b> ${e.comments}</li>
           <li class="text-content"><b>Downloads:</b> ${e.downloads}</li>
         </ul>
+        
       </a>
   `).join("");n.innerHTML=o,l?l.refresh():l=new u(".gallery a",{captionsData:"alt",captionDelay:250,captionPosition:"bottom"})}function h(){const t=document.querySelector(".gallery");t.innerHTML=""}function m(t){a.info({title:"Info",message:t,position:"topRight",backgroundColor:"red",messageColor:"white",titleColor:"white"})}const g="https://pixabay.com/api/",y="46020556-902bd777cf965228c32d535d7";function p(t){const n=new URLSearchParams({key:y,q:t,image_type:"photo",orientation:"horizontal",safesearch:"true",per_page:15}),o=`${g}?${n.toString()}`;return fetch(o).then(e=>{if(!e.ok)throw new Error(`HTTP error! Status: ${e.status}`);return e.json()}).then(e=>(console.log(e),e)).catch(e=>{throw console.error("There was an error with the fetch operation:",e),e})}const b=document.querySelector(".search-form"),L=document.querySelector('[name="searchQuery"]'),w=document.querySelector(".gallery");b.addEventListener("submit",t=>{t.preventDefault();const n=L.value.trim();if(!n){a.error({title:"Error",message:"Please enter a search query.",position:"topRight"});return}h(),f(w),p(n).then(o=>{if(c(),!o||o.hits.length===0){m("Sorry, there are no images matching your search query. Please try again!");return}d(o.hits)}).catch(o=>{console.error("Error fetching images:",o),a.error({title:"Error",message:`Error: ${o.message}`,position:"topRight"})}).finally(()=>{c()})});
 //# sourceMappingURL=index.js.map
